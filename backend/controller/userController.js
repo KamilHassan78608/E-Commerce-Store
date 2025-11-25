7b02789c5c678ec6404adbda3ab203e256cb6d19
mac <mac@192.168.1.10>
2025-11-25 21:55:26 +0500

    API completed and Setup for frontend of Admin

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -47,6 +47,7 @@ const login_User = async  (req, res) => {
     
 }
 
+
 // Routes for user Register
 const register_User = async (req, res) => {
     try {
@@ -105,7 +106,29 @@ const register_User = async (req, res) => {
 
 // Route for admin Login
 const admin_Login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email+password, process.env.JWT_SECRET);
+            res.json({
+                success: true,
+                token
+            })
+        }else{
+            res.json({
+                success: false,
+                message: "Invalid Credentials"
+            })
+        }
 
+    } catch (error) {
+        console.log(error);
+        res.json({
+            success: false,
+            messege: error.messege
+        })
+    }
 }
 
 export { login_User, register_User, admin_Login }
+
